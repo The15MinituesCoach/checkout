c2eec602a928c41462788e5475cb882621e2e25c
The15MinituesCoach <conscogindia@gmail.com>
2025-05-11 19:24:22 +0530

    Add files via upload

---
+++ server/storage.ts
@@ -0,0 +1,31 @@
+import type { User, InsertUser } from "@shared/schema";
+
+// Storage interface
+export interface IStorage {
+  getUser(id: number): Promise<User | undefined>;
+  getUserByUsername(username: string): Promise<User | undefined>;
+  createUser(user: InsertUser): Promise<User>;
+}
+
+// In-memory implementation
+export class MemStorage implements IStorage {
+  private users: Map<number, User> = new Map();
+  private currentId = 1;
+
+  async getUser(id: number): Promise<User | undefined> {
+    return this.users.get(id);
+  }
+
+  async getUserByUsername(username: string): Promise<User | undefined> {
+    return Array.from(this.users.values()).find((user) => user.username === username);
+  }
+
+  async createUser(insertUser: InsertUser): Promise<User> {
+    const id = this.currentId++;
+    const user: User = { ...insertUser, id };
+    this.users.set(id, user);
+    return user;
+  }
+}
+
+export const storage = new MemStorage();
