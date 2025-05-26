53d3e1ba99c109d4f42178569f47d4dc39dcfdb7
The15MinituesCoach <conscogindia@gmail.com>
2025-05-26 13:23:24 +0530

    Add files via upload

---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,36 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import ProblemRecognition from "@/components/ProblemRecognition";
+import Solution from "@/components/Solution";
+import Promotion from "@/components/Promotion";
+import OfferBreakdown from "@/components/OfferBreakdown";
+import HowItWorks from "@/components/HowItWorks";
+import Testimonials from "@/components/Testimonials";
+import FAQ from "@/components/FAQ";
+import Guarantee from "@/components/Guarantee";
+import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
+import MobileStickyCTA from "@/components/MobileStickyCTA";
+import MindfulnessPopup from "@/components/MindfulnessPopup";
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-background">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <ProblemRecognition />
+        <Solution />
+        <OfferBreakdown />
+        <HowItWorks />
+        <Promotion />
+        <Testimonials />
+        <FAQ />
+        <Guarantee />
+        <FinalCTA />
+      </main>
+      <Footer />
+      <MobileStickyCTA />
+       <MindfulnessPopup /> 
+    </div>
+  );
+}
